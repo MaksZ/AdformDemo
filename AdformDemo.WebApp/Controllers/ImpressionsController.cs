5a4378d63e766a86cd1f7b65020b6b5d757d2d45
Maksim Zavidejev <maksim.zavidejev@gmail.com>
2018-03-25 01:09:14 +0200

    WebApi added

---
+++ AdformDemo.WebApp/Controllers/ImpressionsController.cs
@@ -0,0 +1,22 @@
+﻿using AdformDemo.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AdformDemo.WebApp.Controllers
+{
+    public class ImpressionsController : ApiController
+    {
+        // For demo purposes we store sensitive data as plain.
+        const string clientId = "sellside.apiteam@tests.adform.com";
+        const string clientSecret = "xPDUpHFZHuobERbKVjVxPujndfyg4C6KLDItwLwK";
+
+        public IEnumerable<WeekImpressions> GetItems()
+        {
+            return new Repository(clientId, clientSecret).ImpressionsAsync.Result.GroupByWeek();
+        }
+    }
+}
